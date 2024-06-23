80f9f0878c430dd6732907b0c7265bc24650e906
Elvin Lee <z5309579@ad.unsw.edu.au>
2024-06-24 03:08:40 +1000

    navbar and some login page development

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    standalone: true
+    styleUrl: './login.component.css',
+    standalone: true,
+    imports: [ MatButtonModule, MatCardModule, MatInputModule ]
 })
 export class LoginComponent {}
